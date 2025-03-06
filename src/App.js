34e11e3bcfb7c27b05f3311b46003ca6844a334e
unknown <prakharchaturvedi9860@gmail.com>
2025-03-06 15:12:09 +0530

    created first draft of the working application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [country, setCountry] = useState("");
+  const [countryList, setCountryList] = useState([]);
+  const [stateList, setStateList] = useState([]);
+  const [state, setState] = useState("");
+  const [cityList, setCityList] = useState([]);
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      try {
+        const response = await axios.get(
+          ` https://crio-location-selector.onrender.com/countries`
+        );
+        setCountryList(response.data);
+        
+      } catch (error) {
+        console.error("Error fetcing data:", error);
+      }
+    };
+    fetchCountry();
+  }, []);
+
+  useEffect(() => {
+    setState("");
+    setCity("");
+    setCityList([]);
+
+    if (country !== "") {
+      const fetchStates = async () => {
+        try {
+          const response = await axios.get(
+            `https://crio-location-selector.onrender.com/country=${country}/states`
+          );
+          setStateList(response.data);
+        } catch (error) {
+          console.error("Error fetcing data:", error);
+        }
+      };
+      fetchStates();
+    } else {
+      setStateList([]);
+    }
+  }, [country]);
+
+  //fetch city
+  useEffect(() => {
+    setCity("");
+
+    if (country && state && state.trim() !== "" && country.trim() !== "") {
+      const fetchCity = async () => {
+        try {
+          const response = await axios.get(
+            `https://crio-location-selector.onrender.com/country=${country}/state=${state}/cities`
+          );
+          setCityList(response.data);
+        } catch (error) {
+          console.error("Error fetcing data:", error);
+        }
+      };
+      fetchCity();
+    }else{
+      setCityList([])
+    }
+  }, [state, country]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2 style={{ textAlign: "center" }}>Select Location</h2>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <select
+          style={{
+            width: "400px",
+            height: "30px",
+            textAlign: "center",
+            margin: "5px",
+          }}
+          value={country}
+          onChange={(e) => {
+            const selectedCountry = e.target.value;
+            setCountry(selectedCountry);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="" disabled>
+            Select Country
+          </option>
+          {countryList.map((item) => (
+            <option key={item}>{item}</option>
+          ))}
+        </select>
+        {/* City */}
+        <select
+          style={{
+            width: "250px",
+            height: "30px",
+            textAlign: "center",
+            margin: "5px",
+          }}
+          value={state}
+          onChange={(e) => {
+            const selectedState = e.target.value;
+            setState(selectedState);
+          }}
+        >
+          <option value="" disabled>
+            Select state
+          </option>
+          {stateList.map((item) => (
+            <option key={item}>{item}</option>
+          ))}
+        </select>
+        <select
+          style={{
+            width: "250px",
+            height: "30px",
+            textAlign: "center",
+            margin: "5px",
+          }}
+          value={city}
+          onChange={(e) => {
+            const selectedCity = e.target.value;
+            setCity(selectedCity);
+          }}
+        >
+          <option value="" disabled>
+            Select city
+          </option>
+          {cityList.map((item) => (
+            <option key={item}>{item}</option>
+          ))}
+        </select>
+      </div>
+      {country && state && city ? (
+        <div style={{textAlign:'center', marginTop:'30px'}}> 
+          <b>You selected : {city},</b> {state}, {country}
+        </div>
+      ) : null}
     </div>
   );
 }
